31888b85f4e0ac274cae9ca9cf447e33bf1fd9c3
Iman <ThePunisher1383@gmail.com>
2025-04-18 01:39:04 +0330

    update mongodb connection

--- api edited/server.js
+++ api edited/server.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();  // بارگذاری متغیرهای محیطی از فایل .env
 
-const userRoutes= require('./routes/user-routes');
+const userRoutes = require('./routes/user-routes');
 const bookRoutes = require('./routes/book');
 const reviewRoutes = require('./routes/review');
 
 const app = express();
 app.use(express.json());
-app.use('/kaka',userRoutes);
-app.use('/kaka',bookRoutes);
-app.use('/kaka',reviewRoutes);
-app.get('/',(req,res)=>{
+app.use('/kaka', userRoutes);
+app.use('/kaka', bookRoutes);
+app.use('/kaka', reviewRoutes);
+
+app.get('/', (req, res) => {
     res.send("hello world!!");
-})
+});
+
+// اتصال به MongoDB Atlas
 mongoose
-    .connect('mongodb://127.0.0.1:27017/Bookrecommendtion')
+    .connect(process.env.MONGO_URI)  // استفاده از متغیر محیطی
     .then(() => {
-        app.listen(8000);
-        console.log('connected');
+        app.listen(8000, () => {
+            console.log('Server is running on port 8000');
+        });
+        console.log('Connected to MongoDB Atlas');
     })
     .catch((err) => {
         console.log(err);
-    })
+    });
